5a8d794cc28586b6ac6f1bfaa2859f9261c2439b
HelloWorld-n <gajica.b@gmail.com>
2022-07-08 08:48:00 +0200

    modified:   a.out 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,19 @@
 #include <chrono>
 #include <thread>
 
-const int screenWidth = 800;
-const int screenHeight = 600;
+const int screenWidth = 760;
+const int screenHeight = 360;
 const SDL_Color bg = {0, 0, 0, 255};
 const SDL_Color fg = {196, 196, 196, 255};
+const SDL_Color sdl_blue = {64, 64, 255, 255};
+const SDL_Color sdl_yellow = {228, 228, 32, 255};
+const int fontSize_main = 24;
+const int fontSize_side = 12;
 
 void SDL_DrawText(
 	SDL_Renderer *renderer, int x, int y, char *text,
-	TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect
+	TTF_Font *font, SDL_Texture **texture, SDL_Rect *rect,
+	SDL_Color bg = ::bg, SDL_Color fg = ::fg
 ) {
 	int text_width;
 	int text_height;
@@ -73,8 +78,8 @@ int main(int argc, char* args[]) {
 	SDL_Texture *texture = NULL;
 
 	TTF_Init();
-	TTF_Font *font_main = TTF_OpenFont(fontPath, 24);
-	TTF_Font *font_side = TTF_OpenFont(fontPath, 12);
+	TTF_Font *font_main = TTF_OpenFont(fontPath, fontSize_main);
+	TTF_Font *font_side = TTF_OpenFont(fontPath, fontSize_side);
 	SDL_Surface *surfaceMessage = TTF_RenderText_Solid(font_main, getLocalTime(time(0)).c_str(), fg); 
 	SDL_Rect rect;
 	
@@ -100,11 +105,22 @@ int main(int argc, char* args[]) {
 		std::string the_now_local = getLocalTime(time(0));
 		std::string the_now_global = getGlobalTime(time(0));
 		
+		the_now_global.pop_back();
+		the_now_global.append("(UTC)");
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         SDL_RenderClear(renderer);
-		SDL_DrawText(renderer, 10, 0, ( char * ) the_now_local.c_str(), font_main, &texture, &rect);
+		SDL_DrawText(
+			renderer, 10, (screenHeight / 2) - fontSize_main, ( char * ) the_now_local.c_str(), 
+			font_main, &texture, &rect,
+			bg, sdl_blue
+		);
 		SDL_RenderCopy(renderer, texture, NULL, &rect);
-		SDL_DrawText(renderer, 10, 30, ( char * ) the_now_global.c_str(), font_side, &texture, &rect);
+		SDL_DrawText(
+			renderer, 10, screenHeight / 2, ( char * ) the_now_global.c_str(), 
+			font_main, &texture, &rect,
+			bg, sdl_yellow
+		);
 		SDL_RenderCopy(renderer, texture, NULL, &rect);
         SDL_RenderPresent(renderer);
 	
